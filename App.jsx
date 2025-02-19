3648b55e1cf90c16d1adf3e744f93b5154ab93d3
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-19 13:03:53 -0700

    Mapped dice array to Dice React components

--- App.jsx
+++ App.jsx
@@ -1,19 +1,18 @@
 import Die from './components/Die';
+import { useState } from 'react';
 
 export default function App() {
+    const [diceArr, setDiceArr] = useState(generateAllNewDice());
+    function generateAllNewDice() {
+        return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    }
+
     return (
         <main>
             <div className="dice-container">
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
+                {diceArr.map((val) => (
+                    <Die value={val} />
+                ))}
             </div>
         </main>
     );
