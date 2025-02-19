c86db61ccc005a87c8e59f9ce11938526022536b
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-19 12:49:11 -0700

    Added die component

--- App.jsx
+++ App.jsx
@@ -1,3 +1,20 @@
+import Die from './components/Die';
+
 export default function App() {
-    return <main>I'm some text</main>;
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+        </main>
+    );
 }
