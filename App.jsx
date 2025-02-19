32a4b9ee4248ba174e61af1d1c246c2c876a8f1a
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-19 16:06:38 -0700

    Added confetti if user wins Tenzies

--- App.jsx
+++ App.jsx
@@ -1,19 +1,9 @@
 import Die from './components/Die';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import ReactConfetti from 'react-confetti';
 
 export default function App() {
-    /**
-     * Challenge: Create a function `hold` that takes
-     * `id` as a parameter. For now, just have the function
-     * console.log(id).
-     *
-     * Then, figure out how to pass that function down to each
-     * instance of the Die component so when each one is clicked,
-     * it logs its own unique ID property. (Hint: there's more
-     * than one way to make that work, so just choose whichever
-     * you want)
-     */
     const [dice, setDice] = useState(generateAllNewDice());
     function generateAllNewDice() {
         return new Array(10).fill(0).map(() => ({
@@ -57,6 +47,7 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <ReactConfetti />}
             <header className="header">
                 <h1 className="title">Tenzies</h1>
                 <p>
